41ff0a7621b7c184dc33d0cfe70cb147d505a5f7
Luís Teixeira <116684878+1190835@users.noreply.github.com>
2024-02-19 17:28:57 +0000

    padlock, n ta a dar :/

--- Assets/Scripts/PadlockController.cs
+++ Assets/Scripts/PadlockController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PadlockController : MonoBehaviour
@@ -10,7 +11,7 @@ public class PadlockController : MonoBehaviour
     //Numero de slots q pararam de rodar
     public int stoppedIdx =0;
     float speed = 0.01f;
-    private float[] faceRotations = {72, 36, 0, 324, 288, 252, 216, 180, 144, 108};
+    private float [] faceRotations = {108, 144, 180, 216, 252, -72, -36, 0, 36, 72};
     private int[] combination = {1,2,3,4,5};
     public int[] input = new int[5];
 
@@ -22,15 +23,10 @@ public class PadlockController : MonoBehaviour
             rounds[i].transform.rotation = Quaternion.Lerp(rounds[i].transform.rotation, rounds[i].transform.rotation * Quaternion.Euler(90, 0, 0), speed);
             
         }
-        //Se um slot esta parado arredonda a rotation para o multiplo mais proximo de 36, para que uma face esteja sempre virada para
-        // a camara 
-        for(int i =0; i<stoppedIdx;i++){
-            Vector3 rotate = rounds[i].transform.rotation.eulerAngles;
-            rotate = new Vector3((float)(Math.Round(rotate.x / 36) * 36), rotate.y, rotate.z);
-            rounds[i].transform.rotation = Quaternion.Euler(rotate);
-        }
+        
 
         if(Input.GetKeyDown(KeyCode.Mouse0) && stoppedIdx<=4){
+            alignRound(stoppedIdx);
             stoppedIdx++;
         }
         if(stoppedIdx>4){
@@ -38,10 +34,18 @@ public class PadlockController : MonoBehaviour
         }
     }
 
+    private void alignRound(int i){
+            //Se um slot esta parado arredonda a rotation para o multiplo mais proximo de 36, para que uma face esteja sempre virada para
+            // a camara 
+            Vector3 rotate = rounds[i].transform.rotation.eulerAngles;
+            rotate = new Vector3((float)(Math.Round(rotate.x / 36) * 36), rotate.y, rotate.z);
+            rounds[i].transform.rotation = Quaternion.Euler(rotate);
+    }
+
     private bool checkCode(){
         for(int i =0;i<5;i++){
-            input[i] = Array.IndexOf(faceRotations,(float)Math.Round(rounds[i].transform.rotation.eulerAngles.x));
-            Debug.Log((float)Math.Round(rounds[i].transform.rotation.eulerAngles.x));
+            input[i] = Array.IndexOf(faceRotations,(float)Math.Round(rounds[i].transform.localRotation.x));
+            Debug.Log((float)Math.Round(rounds[i].transform.localRotation.x*360));
         }
         foreach(int i in input){
             Debug.Log(i.ToString());
