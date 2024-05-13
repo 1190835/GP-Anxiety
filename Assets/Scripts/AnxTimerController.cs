fd9fb2fc09934ed1f185733d41f1cf32bccb7688
Luís Teixeira <116684878+1190835@users.noreply.github.com>
2024-05-13 23:11:52 +0100

    timer finished

--- Assets/Scripts/AnxTimerController.cs
+++ Assets/Scripts/AnxTimerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,12 @@ using UnityEngine.UI;
 
 public class AnxTimerController : MonoBehaviour
 {
+    private float time;
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.GetComponent<Text>().text = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().anxTimer.ToString();
+        time = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().anxTimer;
+        string str = TimeSpan.FromSeconds((double)time).ToString(@"m\:ss");
+        this.gameObject.GetComponent<Text>().text = str;
     }
 }
